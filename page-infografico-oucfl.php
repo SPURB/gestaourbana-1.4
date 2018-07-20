79f234d5ffaadb80b2358b082d054a3580d49493
yubathom <yubathom@gmail.com>
2018-07-20 14:01:42 -0300

    Adiciona loader para infografico-oucae e infografico-oucfl

--- page-infografico-oucfl.php
+++ page-infografico-oucfl.php
@@ -4,12 +4,14 @@ Template Name: infografico-oucfl
 */
 ?>
 <?php get_header(); ?>
+<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/style-loader.css">
 <link rel="stylesheet" type="text/css" href="<?php  echo get_template_directory_uri(); ?>/SPA/infograficos/dist/oucfl.min.css">
 
 <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
 	<div id="default-inner">
 		<div class="wrapper">
 			<div class="inner">
+				<div id="preloader">Carregando...</div>
 				<div class="text">
 					<h3><?php echo the_title(); ?></h3>
 					<div class="inner-text">
@@ -23,6 +25,12 @@ Template Name: infografico-oucfl
 <?php endwhile; ?>
 
 <script src="<?php echo get_template_directory_uri() .'/js/vue.v2.5.16.js';?>"></script>
-<script src="<?php echo get_template_directory_uri() .'/SPA/infograficos/dist/oucfl.min.js';?>" defer></script>
+<script src="<?php echo get_template_directory_uri() .'/SPA/infograficos/dist/oucfl.min.js';?>"></script>
+<script type="text/javascript" defer>
+var loader = document.getElementById('preloader');
+jQuery(document).ready(function() {
+	loader.style.display = 'none';
+})
+</script>
 
 <?php get_footer(); ?>

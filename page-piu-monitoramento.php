94967688a1c7b96ebee14989805e0d6a4c9e57b6
yubathom <yubathom@gmail.com>
2018-08-07 18:49:51 -0300

    SPA/piu-monitoramento - separacao completa dos dados

--- page-piu-monitoramento.php
+++ page-piu-monitoramento.php
@@ -15,9 +15,9 @@ Template Name: piu-monitoramento
 	<div class="wrapper">
 			<div class="inner">
 				<div id="app">
-					<mapa		v-show="display.mapa"		@clicked="receiveId"	:clicked-id="projectId">    </mapa>
-					<sumario	v-show="display.sumario"	@clicked="receiveId"></sumario>
-					<ficha		v-show="display.ficha"		@clicked="receiveId"	:clicked-id="projectId">    </ficha>
+					<mapa 	 	v-show="display.mapa"		@clicked="receiveId"	:clicked-id="projectId" 	:data='monitoramento'></mapa>
+					<sumario 	v-show="display.sumario"	@clicked="receiveId" 								:data='monitoramento' 	:hiperlinks='hiperlinks'></sumario>
+					<ficha 		v-show="display.ficha" 		@clicked="receiveId"	:clicked-id="projectId" 	:data='monitoramento' 	:hiperlinks='hiperlinks'></ficha>
 				</div>
 			</div>
 		<div class="clear"></div>
@@ -27,7 +27,7 @@ Template Name: piu-monitoramento
 <?php get_footer(); ?>
 
 <script type="text/javascript">
-	var dist_folder = "<?php echo get_template_directory_uri(); ?>" + "/SPA/piu-monitoramento/dist/";
+var dist_folder = "<?php echo get_template_directory_uri(); ?>" + "/SPA/piu-monitoramento/dist/";
 </script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/vue.v2.5.16.js"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/ol.4.2.0.js"></script>
